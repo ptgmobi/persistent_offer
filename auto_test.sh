7cfe2b298b8760d8f5b8746f3491fe973124bda2
董家宏 <dongjiahong@hotmail.com>
2017-02-07 11:03:32 +0800

    modify auto_test.sh

--- auto_test.sh
+++ auto_test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+PWD=$(pwd)
+COVFILE=${PWD}/coverage.txt
+
 function test_and_append_coverage() {
 	pushd $1 > /dev/null
 	go test -v -coverprofile=profile.out -covermode=atomic
@@ -9,9 +12,6 @@ function test_and_append_coverage() {
 	fi
 	popd > /dev/null
 }
-
-PWD=$(pwd)
-COVFILE=${PWD}/coverage.txt
 
 set -e
 echo "" > ${COVFILE}
