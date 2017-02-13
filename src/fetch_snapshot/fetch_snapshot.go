2492d6b8a9b624ff2cda84126a0d56f98e9df2d0
董家宏 <dongjiahong@hotmail.com>
2017-02-13 19:29:17 +0800

    fix bug

--- src/fetch_snapshot/fetch_snapshot.go
+++ src/fetch_snapshot/fetch_snapshot.go
@@ -126,11 +126,10 @@ func NewService(conf *Conf, dbConf *dbCore.Conf) *Service {
 
 // Server 该函数准备新表,调用入库数据，并删除旧表
 func (s *Service) Server() {
-	{
-		s.apiLock.Lock()
-		s.domain_api = s.conf.FetchApi // 保存domain形式的api
-		s.apiLock.Unlock()
-	}
+
+	s.apiLock.Lock()
+	s.domain_api = s.conf.FetchApi // 保存domain形式的api
+	s.apiLock.Unlock()
 
 	go s.checkIpApi()
 
@@ -183,21 +182,17 @@ func (s *Service) checkIpApi() {
 			}
 			if err != nil {
 				if (apiNum+1) == len(s.conf.FetchIpApi) { // 最后一个ipapi也不行还是用域名吧！
-					{
-						s.apiLock.Lock()
-						s.conf.FetchApi = s.domain_api
-						s.apiLock.Unlock()
-					}
+					s.apiLock.Lock()
+					s.conf.FetchApi = s.domain_api
+					s.apiLock.Unlock()
 					s.l.Println("checkIpApi ipapi don't work use domain api")
 				}
 				continue
 			}
 			if resp.StatusCode == 200 {
-				{
-					s.apiLock.Lock()
-					s.conf.FetchApi = s.conf.FetchIpApi[apiNum]
-					s.apiLock.Unlock()
-				}
+				s.apiLock.Lock()
+				s.conf.FetchApi = s.conf.FetchIpApi[apiNum]
+				s.apiLock.Unlock()
 				s.l.Println("checkIpApi use ipapi: ", s.conf.FetchIpApi[apiNum])
 				break
 			}
@@ -250,11 +245,10 @@ func (s *Service) getDeleteTableSqlQuery(tableName string) string {
 }
 
 func (s *Service) fetchSnapshot(tableName string) error {
-	{
-		s.apiLock.Lock()
-		fetchApi := s.conf.FetchApi
-		s.apiLock.Unlock()
-	}
+	s.apiLock.Lock()
+	fetchApi := s.conf.FetchApi
+	s.apiLock.Unlock()
+
 	if len(s.conf.FetchApi) == 0 || len(tableName) == 0 {
 		s.l.Println(" >>>>> FetchApi is nil or tableName is nil, api: ", s.conf.FetchApi,
 			" tableName: ", tableName)
