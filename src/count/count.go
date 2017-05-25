c1c9a768b48f9f1f92d5b5fdcba578277bf88bb5
jiahong.dong <dongjiahong@hotmail.com>
2017-05-25 16:41:19 +0800

    Develop (#32)
    * 统计v2.0

--- src/count/count.go
+++ src/count/count.go
@@ -15,26 +15,43 @@ import (
 )
 
 type Count struct {
-	Id         string // ym_123
 	UpdateDate int    // 20170809
-	UpdateHour string // 0-23
 	Channel    string // ym|tym|adst|irs...
 	Platform   string // iOS|Android
-	Country    string // CN|US|JP...
+	Countries  []string
 	PkgName    string // api.com
+	Video      bool   // 是否有视频素材
+}
+
+type OfferRecord struct {
+	Id            int64
+	UpdateDate    int
+	Channel       string
+	Country       string
+	Platform      string
+	VideoOfferNum int // video的offer数量
+	VideoPkgNum   int // video的包名数量
+	OfferNum      int // offer总数
+	PkgNum        int // 包名总数
 }
 
 /*
-create table counts(
-	id char(255) not null comment 'dnfid',
-	update_date char(255) not null comment '更新的日期，年月日',
-	update_hour char(255) not null comment '更新时间，小时',
-	channel char(255) not null comment 'offer的渠道',
-	platform char(255) not null comment 'offer的平台ios|android',
-	country char(255) not null comment 'offer所属国家',
-	unique key(id, update_date, update_hour, country),
-	key `idx_id` (`id`)
-)ENGINE=InnoDB default CHARSET=utf8;
+CREATE TABLE `offer_records` (
+	`id` int(20) NOT NULL AUTO_INCREMENT,
+	`update_date` int(11) NOT NULL COMMENT '更新日期',
+	`channel` char(64) NOT NULL COMMENT 'offer渠道',
+	`country` char(32) NOT NULL COMMENT '国家字母简写',
+	`platform` char(64) NOT NULL COMMENT 'iOS or Android',
+	`video_offer_num` int(11) DEFAULT '0' COMMENT '视频数',
+	`video_pkg_num` int(11) DEFAULT '0' COMMENT '视频包名数',
+	`offer_num` int(11) DEFAULT '0' COMMENT 'offer总数',
+	`pkg_num` int(11) DEFAULT '0' COMMENT 'offer包数',
+	PRIMARY KEY (`id`),
+	KEY `update_date` (`update_date`),
+	KEY `channel` (`channel`),
+	KEY `country` (`country`),
+	KEY `platform` (`platform`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
 
 type RawData struct {
@@ -78,7 +95,7 @@ func getReadDbName(date string) string {
 	return ""
 }
 
-type CFunc func(data *RawData) []*Count
+type CFunc func(data *RawData) *Count
 
 // 获取表中数据
 func getData(table string, cFunc CFunc) ([]*Count, error) {
@@ -94,7 +111,7 @@ func getData(table string, cFunc CFunc) ([]*Count, error) {
 	}
 	for i := 0; i < len(rawDatas); i++ {
 		if record := cFunc(&rawDatas[i]); record != nil {
-			res = append(res, record...)
+			res = append(res, record)
 		}
 	}
 	if len(res) > 0 {
@@ -103,52 +120,138 @@ func getData(table string, cFunc CFunc) ([]*Count, error) {
 	return nil, errors.New("can't get data")
 }
 
-// 插入新的统计表
-func insertData(records []*Count) {
+func adjustRecords(records []*Count) []*OfferRecord {
+	res := make([]*OfferRecord, 0, 2048)
+	// 整理数据
+	adjust := make(map[string][]*Count, 2048) // [ym_ios_cn][*count, *count ...]
 	for _, record := range records {
-		orm.Gdb.NewRecord(record)
-		errs := orm.Gdb.Create(record).GetErrors()
-		if len(errs) > 0 {
-			log.Println("[insertData] insert err: ", errs)
+		keySliceAll := make([]string, 0, 3)
+		keySliceAll = append(keySliceAll, record.Channel)
+		keySliceAll = append(keySliceAll, record.Platform)
+		keySliceAll = append(keySliceAll, "ALL")
+		keyAll := strings.Join(keySliceAll, "_")
+		adjust[keyAll] = append(adjust[keyAll], record)
+
+		for _, country := range record.Countries {
+			keySlice := make([]string, 0, 3)
+			keySlice = append(keySlice, record.Channel)
+			keySlice = append(keySlice, record.Platform)
+			keySlice = append(keySlice, country)
+			key := strings.Join(keySlice, "_")
+
+			adjust[key] = append(adjust[key], record)
 		}
 	}
+	// 统计计数
+	for k, counts := range adjust {
+		var ch, plt, co string
+		if keySlice := strings.Split(k, "_"); len(keySlice) != 3 {
+			log.Println("[adjustRecords] split key err, key: ", k)
+			continue
+		} else {
+
+			ch = keySlice[0]
+			plt = keySlice[1]
+			co = keySlice[2]
+		}
+
+		var offerNum, videoNum int
+		pkgNum := make(map[string]bool, 128)
+		videoPkgNum := make(map[string]bool, 128)
+
+		offerNum = len(counts)
+		for _, c := range counts {
+			if c.Video {
+				videoNum += 1
+				if len(c.PkgName) > 0 {
+					videoPkgNum[c.PkgName] = true
+				}
+			}
+			if len(c.PkgName) > 0 {
+				pkgNum[c.PkgName] = true
+			}
+		}
+		or := &OfferRecord{
+			UpdateDate:    counts[0].UpdateDate,
+			Channel:       ch,
+			Country:       co,
+			Platform:      plt,
+			VideoOfferNum: videoNum,
+			VideoPkgNum:   len(videoPkgNum),
+			OfferNum:      offerNum,
+			PkgNum:        len(pkgNum),
+		}
+		res = append(res, or)
+	}
+	return res
+}
+
+// 插入新的统计表
+func insertData(records []*Count) {
+	ors := adjustRecords(records)
+	for _, or := range ors {
+		orm.Gdb.NewRecord(or)
+		errs := orm.Gdb.Create(or).GetErrors()
+		if len(errs) > 0 {
+			log.Println("[insertData] insert offer records err: ", errs)
+		}
+	}
+
+}
+
+func delOldData() {
+	old := time.Now().UTC().AddDate(0, 0, -31).Format("2006010215") // 删除3周前的
+	//old := time.Now().Format("2006010215") // 删除3周前的
+	date, err := strconv.Atoi(old)
+	if err != nil {
+		log.Println("[delOldData] get date err: ", err)
+		return
+	}
+	x := orm.Gdb.Delete(OfferRecord{}, "update_date < ?", date)
+	errs := x.GetErrors()
+	if len(errs) > 0 {
+		log.Println("[delOldData] delete old data err: ", errs)
+	}
+	log.Println("[delOldData] delete records num: ", x.RowsAffected)
 }
 
 func Server() {
 	// 插入数据一小时一次就好
 	for {
-		t := time.Now().UTC()
+		t := time.Now()
+		//t := time.Now().UTC()
 		date := t.Format("2006010215")
 		hour := date[8:10]
 
 		var table = getReadDbName(date)
+		log.Println("-----------> table: ", table)
 
 		if table != "" {
-			if records, err := getData(table, func(data *RawData) []*Count {
-				raws := make([]*Count, 0, 8)
+			if records, err := getData(table, func(data *RawData) *Count {
 				var content fs.Offer
 				err := json.Unmarshal(data.Content, &content)
 				if err != nil {
 					log.Println("[Serve] get content err: ", err)
 					return nil
 				}
-				for _, country := range content.Attr.Countries {
-					var c Count
-					c.Channel = data.Channel
-					dateInt, _ := strconv.Atoi(date)
-					c.UpdateDate = dateInt
-					c.UpdateHour = hour
-					c.Id = data.DocId
-					c.Platform = content.Attr.Platform
-					c.Country = country
-					c.PkgName = content.Attr.AppDown.AppPkgName
-					raws = append(raws, &c)
+
+				var c Count
+				c.Channel = data.Channel
+				dateInt, _ := strconv.Atoi(date)
+				c.UpdateDate = dateInt
+				c.Platform = content.Attr.Platform
+				c.Countries = append(c.Countries, content.Attr.Countries...)
+				c.PkgName = content.Attr.AppDown.AppPkgName
+				if strings.Contains(content.Dnf, "video,") {
+					c.Video = true
 				}
-				return raws
+
+				return &c
 			}); err != nil {
 				log.Println("get data err: ", err)
 			} else {
 				insertData(records)
+				//delOldData()
 			}
 		} else {
 			log.Println("[Serve] can't get table name!")
